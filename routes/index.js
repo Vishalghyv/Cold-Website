bc62ad9e7032225190b4cb2460afdd51c3e25e67
dravadhis <shubham25powertech@gmail.com>
2020-11-05 11:14:56 +0530

    Separate header, navbar and footer views

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,12 @@ const router = express.Router()
 router.get('/', (req,res) => {
     res.render('home')
 })
+router.get('/projects', (req,res) => {
+    res.render('projects')
+})
+router.get('/events', (req,res) => {
+    res.render('events')
+})
+
 
 module.exports = router
