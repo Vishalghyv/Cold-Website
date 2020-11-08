38ff8679aa21b4321a7e8daadf97eca6f0c9a3a0
Vishalghyv <vishal38785@gmail.com>
2020-11-08 19:07:08 +0530

    Database, Admin register, login and verification api done

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports ={
+    mongoURI: 'mongodb+srv://vishal:vishal@cluster0.jcfqk.mongodb.net/COLD?retryWrites=true&w=majority'
+}
--- server.js
+++ server.js
@@ -5,8 +5,23 @@ if(process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const passport = require("passport")
 
 const indexRouter = require('./routes/index')
+const userRouter = require('./routes/api/Users')
+
+const bodyParser = require('body-parser')
+
+const mongoose = require("mongoose")
+const connectDB = require('./DB/connect')
+connectDB()
+
+
+//Passport middleware
+app.use(passport.initialize())
+
+require('./config/passport.js')(passport)
+
 
 
 app.set('view engine', 'ejs')
@@ -15,17 +30,13 @@ app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
-const mongoose = require("mongoose")
-mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true
-})
-
-const db = mongoose.connection
-db.on('error', error => console.log(error))
-db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
+
+app.use('/api/user', userRouter)
 
 app.listen(process.env.PORT || 3000)
 
